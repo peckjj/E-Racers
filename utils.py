947488c31200329ddc1296088ef3341a164a979c
Joshua Peck <peckjj@dukes.jmu.edu>
2017-12-06 19:04:35 -0500

    Initial Commit of Color class in Utils module

---
+++ utils.py
@@ -0,0 +1,20 @@
+class color(object):
+    WHITE = (255,255,255)
+    BLACK = (0,0,0)
+    RED = (255,0,0)
+    ORANGE = (255,160,122)
+    YELLOW = (255,255,0)
+    GREEN = (0,255,0)
+    BLUE = (0,0,255)
+    PURPLE = (128,0,128)
+    
+    def __init__(self, r, g, b):
+        self.r = r
+        self.g = g
+        self.b = b
+
+    def getColor(self):
+        print([self.r, self.g, self.b])
+        return (self.r,self.g,self.b)
+    
+        
