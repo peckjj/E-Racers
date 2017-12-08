4dafe9b159cbe9eddd6be480a37d497064d4c931
Joshua Peck <peckjj@dukes.jmu.edu>
2017-12-07 21:39:47 -0500

    MAJOR UPDATE: FINAL RELEASE FOR PROTOTYPE - added precise collision managers, improved speed of cars and CPU AI navigation, changed the color of the cars to pink to resemble erasers

--- Actors.py
+++ Actors.py
@@ -1,34 +1,63 @@
 import pygame
 from utils import *
 import math
+import random
 
 class Car(pygame.sprite.Sprite):
     IMAGE = image("Car.png", [0,0])
     LEFT = 1
     RIGHT = 2
+    pink = Color(255,105,180)
     
     def __init__ (self, isPlayer, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.isPlayer = isPlayer
         self.angle = math.pi
-        self.xSpeed = math.cos(self.angle)
-        self.ySpeed = math.sin(self.angle)
+        self.speed = 1
+        self.xSpeed = self.speed * math.cos(self.angle)
+        self.ySpeed = self.speed * math.sin(self.angle)
         self.x = x
         self.y = y
         self.image = pygame.image.load("Car.png")
         self.radius = 15
 
     def update(self):
-        self.xSpeed = math.cos(self.angle)
-        self.ySpeed = math.sin(self.angle)
+        self.xSpeed = self.speed * math.cos(self.angle)
+        self.ySpeed = self.speed * math.sin(self.angle)
+
+    def randomUpdate(self):
+        rand = random.random()
+        if rand < .0025:
+            self.angle = self.angle + 1
+        elif rand < .015:
+            self.angle = self.angle - 1
+        self.update()
+
+    def move(self):
         self.x = self.x + self.xSpeed
         self.y = self.y + self.ySpeed
 
+    def stop(self):
+        self.xSpeed = 0
+        self.ySpeed = 0
+
     def getSpeed(self):
         return self.speed
+
+    def getXSpeed(self):
+        return self.xSpeed
+
+    def getYSpeed(self):
+        return self.ySpeed
     
     def getAngle(self):
         return self.angle
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
     
     def getPosition(self):
         return self.position
@@ -41,9 +70,15 @@ class Car(pygame.sprite.Sprite):
     
     def isPlayer(self):
         return self.isPlayer
-    
+
     def setSpeed(self, speed):
         self.speed = speed
+    
+    def setXSpeed(self, speed):
+        self.xSpeed = speed
+
+    def setYSpeed(self, speed):
+        self.ySpeed = speed
         
     def setAngle(self, angle):
         self.angle = angle
@@ -52,5 +87,5 @@ class Car(pygame.sprite.Sprite):
         self.position = pos
 
     def show(self, display):
-        pygame.draw.circle(display, (255,0,0), [math.floor(self.x), math.floor(self.y)], self.radius, 0)
+        pygame.draw.circle(display, Car.pink.getColor(), [math.floor(self.x), math.floor(self.y)], self.radius, 0)
 
--- ERacers.py
+++ ERacers.py
@@ -1,3 +1,17 @@
+'''
+    ---------------------------------------------------------------------------------------
+    Joshua Peck and Grant Schowalter Official Prototype Release **Pencil E-Racers** v 0.0.1
+    ---------------------------------------------------------------------------------------
+
+    Last update: 12 / 7 / 2017
+
+    ** Made using pygame, a graphics library and framework for python to give
+    game-development tools to anyone who is between new to moderately skilled at coding
+    in python
+
+    www.pygame.org
+'''
+
 import pygame
 from utils import *
 from Actors import *
@@ -10,11 +24,13 @@ FRAMERATE = 200
 fillColor = Color(255, 0, 0)
 width = 800
 height = 800
-size = (800, 800)
+size = (width, height)
 display = pygame.display.set_mode(size)
 
 player = Car(True, 554, 120)
 CPU = Car(False, 554, 230)
+player.setSpeed(4)
+CPU.setSpeed(5)
 
 pygame.init()
 
@@ -34,7 +50,11 @@ def showCars():
 
 def updateCars():
     player.update()
-    CPU.update()
+    CPU.randomUpdate()
+
+def moveCars():
+    player.move()
+    CPU.move()
 
 def eventHandler():
     global ENABLED
@@ -62,12 +82,48 @@ def eventHandler():
         player.setAngle(player.getAngle() - .1)
     if not PAUSED and turnRight:
         player.setAngle(player.getAngle() + .1)
-            
-def canProceed():
-    global player
-    global CPU
-    
-    
+        
+# Inhibits a Car's movement if it drives into (collides with) a region outside
+# the track.
+def canProceed(car):
+    global height
+    global width
+    if car.getY() <= 84 + car.getRadius():
+        car.setYSpeed(1)
+    if car.getY() >= 701 - car.getRadius():
+        car.setYSpeed(-1)
+    if car.getX() <= 78 + car.getRadius():
+        car.setXSpeed(1)
+    if car.getX() >= 730 - car.getRadius():
+        car.setXSpeed(-1)
+    r = car.getRadius()
+    x = car.getX()
+    y = car.getY()
+    '''if x >= 221 - r and y >= 269 - r and y <= 535 + r:
+        if x <= 571 + r:
+            car.setXSpeed(-1)
+    elif x <= 571 + r and y >= 269 - r and y <= 535 + r:
+        if x >= 221 - r:
+            car.setXSpeed(1)
+    if y >= 269 - r and x >= 221 - r and x <= 571 + r:
+        if y <= 535 + r:
+            car.setYSpeed(-1)
+    elif y <= 535 + r and x >= 221 - r and x <= 571 + r:
+        if y >= 269 - r:
+            car.setYSpeed(1)'''
+    if (x >= 221 - r and x <= 571 + r):
+        if (y >= 269 - r and y <= 535 + r):
+            if (y > height / 2):
+                car.setYSpeed(1)
+            else:
+                car.setYSpeed(-1)
+        if (y >= 269 - r and y <= 535 + r):
+            if (x >= 221 - r and x <= 571 + r):
+                if (x > width / 2):
+                    car.setXSpeed(1)
+                else:
+                    car.setXSpeed(-1)
+                    
 #MAIN GAME LOOP
 while ENABLED:
     drawBackground()
@@ -77,6 +133,9 @@ while ENABLED:
     if not PAUSED:
         print("dX: ", player.xSpeed, "  dY: ", player.ySpeed, "  X: ", player.x, "  Y:  ", player.y)
         updateCars()
+        canProceed(player)
+        canProceed(CPU)
+        moveCars()
     clock.tick(FRAMERATE)
 pygame.quit()
 quit()
